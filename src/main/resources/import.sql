9ffea1c7201b4cccbb84a812080911e921fb1260
Mateus Anjos <macedodosanjosmateu@gmail.com>
2025-06-21 23:55:08 -0300

    chore: adiciona arquivos de configuração do Quarkus, incluindo propriedades para ambientes de desenvolvimento, produção e teste, além de mensagens de validação e um script de importação SQL para produtos.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO product(id, sku, name, created_at) VALUES (nextval('hibernate_sequence'), '1234567890', 'Product 1', CURRENT_TIMESTAMP);
+INSERT INTO product(id, sku, name, created_at) VALUES (nextval('hibernate_sequence'), '1234567891', 'Product 2', CURRENT_TIMESTAMP);
+INSERT INTO product(id, sku, name, created_at) VALUES (nextval('hibernate_sequence'), '1234567892', 'Product 3', CURRENT_TIMESTAMP);
