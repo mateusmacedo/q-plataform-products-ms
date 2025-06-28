6d32dff79564d294204c945c968401505afde534
Mateus Anjos <macedodosanjosmateu@gmail.com>
2025-06-28 01:44:33 -0300

    refactor: simplify message sending logic in ProductOutProducer and enhance logging

--- src/main/java/org/acme/producer/ProductOutProducer.java
+++ src/main/java/org/acme/producer/ProductOutProducer.java
@@ -1,7 +1,5 @@
 package org.acme.producer;
 
-import java.nio.charset.StandardCharsets;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -10,44 +8,30 @@ import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
-import org.jboss.logging.MDC;
+import org.jboss.logging.Logger;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
-import lombok.extern.slf4j.Slf4j;
 
 @ApplicationScoped
-@Slf4j
 public class ProductOutProducer {
+    Logger log = Logger.getLogger(ProductOutProducer.class);
 
     @Inject
     @Channel("products-out")
     Emitter<ProductOutputDTO> emitter;
 
     public Uni<Void> send(ProductOutputDTO payload) {
-        String traceId = getOrGenerate("X-Trace-Id");
-        log.info(String.format("[traceId=%s] Enviando mensagem para Kafka: id=%s, sku=%s, nome=%s", traceId, payload.getId(), payload.getSku(), payload.getName()));
-        try {
-            RecordHeaders headers = new RecordHeaders();
-            headers.add("X-Trace-Id", traceId.getBytes(StandardCharsets.UTF_8));
-            emitter.send(
-                Message.of(payload)
-                .addMetadata(
-                    OutgoingKafkaRecordMetadata.builder()
-                        .withHeaders(headers)
-                        .build()
-                )
-            );
-            log.info(String.format("[traceId=%s] Mensagem enviada com sucesso para Kafka: id=%s, sku=%s", traceId, payload.getId(), payload.getSku()));
-        } catch (Exception e) {
-            log.error(String.format("[traceId=%s] Falha ao enviar mensagem para Kafka: id=%s, sku=%s, erro=%s", traceId, payload.getId(), payload.getSku(), e.getMessage()), e);
-            return Uni.createFrom().failure(e);
-        }
-        return Uni.createFrom().voidItem();
-    }
-
-    private String getOrGenerate(String key) {
-        Object value = MDC.get(key);
-        return value != null ? value.toString() : java.util.UUID.randomUUID().toString();
+        return Uni.createFrom().item(() -> {
+            return Message.of(payload)
+                    .addMetadata(
+                            OutgoingKafkaRecordMetadata.builder()
+                                    .withHeaders(new RecordHeaders())
+                                    .build());
+        })
+                .invoke(message -> emitter.send(message))
+                .invoke(() -> log.infof("Enviando produto: %s", payload))
+                .replaceWithVoid()
+                .onFailure().invoke(throwable -> log.errorf("Erro ao enviar produto: %s", throwable.getMessage()));
     }
 }
